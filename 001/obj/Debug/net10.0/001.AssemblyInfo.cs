77a67e21472219eb0c772a43d07adfcc4b818a6a
Vladislav <klimovvladik56@gmail.com>
2026-09-22 22:01:12 +0300

    finishin writing the code

--- 001/obj/Debug/net10.0/001.AssemblyInfo.cs
+++ 001/obj/Debug/net10.0/001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f408532f2d1594a686a1dd81e98b58e77d5b806")]
 [assembly: System.Reflection.AssemblyProductAttribute("001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
